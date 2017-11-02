cb56154abafdb1660c3be2a534203c08c7f87a7b
zhaotang <zhaotang@meizu.com>
2017-11-02 20:11:37 +0800

    test

--- src/main/java/ListMap/PP.java
+++ src/main/java/ListMap/PP.java
@@ -1,4 +1,25 @@
 package ListMap;
 
+import org.assertj.core.util.Arrays;
+
 public class PP {
+
+    public static void main(String[] args) {
+        int[] app={1,33,44,5,6,55,99,54};
+        for (int i=0;i<app.length;i++){
+            for(int j=app.length-1;j>i;j--){
+                if(app[j]>app[j-1]){
+                    int re=app[j-1];
+                    app[j-1]=app[j];
+                    app[j]=re;
+                }
+
+            }
+
+        }
+        for(int a:app){
+            System.out.println(a);
+        }
+    }
+
 }
