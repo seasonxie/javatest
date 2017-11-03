8862708eb9a7f6432aa8b7a2cdb7b8847ba326b3
zhaotang <zhaotang@meizu.com>
2017-11-03 17:26:10 +0800

    f

--- src/main/java/designpatterns/Person.java
+++ src/main/java/designpatterns/Person.java
@@ -1,4 +1,22 @@
 package designpatterns;
 
-public class Person {
+import org.testng.annotations.Test;
+
+public class Person extends SuperClass<Person>{
+   /* public Person() {
+        super();
+    }*/
+
+
+
+    public void function() {
+        System.out.println("function in Person.class...");
+        System.out.println(test);
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("1111111");
+        System.out.println(test);
+    }
 }
